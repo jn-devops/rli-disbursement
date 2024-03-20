6792b5ed70ed67ba15f82174b232e23b2f48cbd4
jn-devops <devops@joy-nostalg.com>
2024-03-20 21:26:15 +0800

    fix transaction via field

--- app/Nova/Transaction.php
+++ app/Nova/Transaction.php
@@ -63,13 +63,11 @@ class Transaction extends Resource
             Text::make('Type')->sortable(),
             MorphTo::make('Payable')->hideFromIndex(),
             Currency::make('Amount')->asMinorUnits()->currency('PHP')->sortable(),
+            Text::make('Via')->sortable(),
 //            Text::make('Via', 'meta->details->settlement_rail')->sortable(),
-            Text::make('Via', function ($attribute) use ($request) {
-                return match($this->getAttribute('type')) {
-                    'withdraw' => $request->json('meta->details->settlement_rail'),
-                    'deposit' =>  Arr::get($this->getAttribute('meta'), 'channel'),
-                };
-            })->sortable(),
+//            Text::make('Via', function ($attribute) use ($request) {
+//                return $this->getAttribute('via');
+//            })->sortable(),
 //            Text::make('Bank', 'meta->details->destination_account->bank_code')->sortable(),
             Text::make('Account Holder', function ($attribute) use ($request) {
                 $holder = match($this->getAttribute('type')) {
